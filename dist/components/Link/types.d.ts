cd6f18ff20367869782b0f106a2c53e236282ecc
tapswap <tapswapmoneydex@gmail.com>
2021-07-03 14:59:55 +0800

    chore: built new patch

--- dist/components/Link/types.d.ts
+++ dist/components/Link/types.d.ts
@@ -3,4 +3,5 @@ import { LinkProps as RRDLinkProps } from "react-router-dom";
 import { TextProps } from "../Text";
 export interface LinkProps extends TextProps, AnchorHTMLAttributes<HTMLAnchorElement>, RRDLinkProps {
     external?: boolean;
+    showIcon?: boolean;
 }
--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -1245,10 +1245,10 @@ Link.defaultProps = {
 var templateObject_1$m;
 
 var LinkExternal = function (_a) {
-    var children = _a.children, props = __rest(_a, ["children"]);
+    var children = _a.children, _b = _a.showIcon, showIcon = _b === void 0 ? false : _b, props = __rest(_a, ["children", "showIcon"]);
     return (React__default['default'].createElement(Link, __assign({ external: true }, props),
         children,
-        React__default['default'].createElement(Icon$F, { color: "primary", ml: "4px" })));
+        showIcon && React__default['default'].createElement(Icon$F, { color: "primary", ml: "4px" })));
 };
 
 var Bar = styled__default['default'].div(templateObject_1$l || (templateObject_1$l = __makeTemplateObject(["\n  position: absolute;\n  top: 0;\n  left: 0;\n  background-color: ", ";\n  border-top-left-radius: 32px;\n  border-bottom-left-radius: 32px;\n  height: 16px;\n  transition: width 200ms ease;\n"], ["\n  position: absolute;\n  top: 0;\n  left: 0;\n  background-color: ", ";\n  border-top-left-radius: 32px;\n  border-bottom-left-radius: 32px;\n  height: 16px;\n  transition: width 200ms ease;\n"])), function (props) { return (props.primary ? props.theme.colors.secondary : props.theme.colors.secondary + "80"); });
